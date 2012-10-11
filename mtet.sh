a196c18a2357d8241d90fd0ceaa4b87850f8e249
Karolin Varner <karo@cupdev.net>
2012-10-11 19:16:43 +0200

    Add support for clang/llvmw

--- mtet.sh
+++ mtet.sh
@@ -5,22 +5,26 @@ STD="c90 c89 iso9899:1990 iso9899:199409 c99 c9x iso9899:1999 iso9899:199x c1x g
 
 # Optimization seems _not_ to affect floats Optimization seems _not_ to affect floats
 # OPTIM="0"
-OPTIM="0 1 2 3 s fast"
+OPTIM="0 1 2 3 s fast z 4"
 
 LANG="c c++"
 
-for _lang_ in $LANG; do
-    for _std_ in $STD; do
-        for _o_ in $OPTIM; do
-            CMD="gcc -O$_o_ --std=$_std_ *.c -o mtet"
-            
-            echo ""                             |  tee -a "$COMPO"
-            echo "############################" |  tee -a "$COMPO"
-            echo "$CMD"                         |  tee -a "$COMPO"
-            echo ""                             |  tee -a "$COMPO"
+COMPILER="gcc clang"
 
-            eval $CMD 2>&1 | tee -a "$COMPO" > /dev/null
-            mtet
+for _comp_ in $COMPILER; do
+    for _lang_ in $LANG; do
+        for _std_ in $STD; do
+            for _o_ in $OPTIM; do
+                CMD="$_comp_ -x$_lang_ -O$_o_ --std=$_std_ *.c -o mtet"
+                
+                echo ""                             |  tee -a "$COMPO"
+                echo "############################" |  tee -a "$COMPO"
+                echo "$CMD"                         |  tee -a "$COMPO"
+                echo ""                             |  tee -a "$COMPO"
+
+                eval $CMD 2>&1 | tee -a "$COMPO" > /dev/null
+                mtet
+            done
         done
     done
 done
